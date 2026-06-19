a000a543963588f43e57e36341347f7df93df209
IgnacioCodingMarathons <ignaciovaselli456@gmail.com>
2026-06-19 12:23:30 -0300

    some altering for the unix setup since grig audio is doin stuff i dont want it to do doode >:(

--- setup/unish-lib.sh
+++ setup/unish-lib.sh
@@ -22,5 +22,6 @@ haxelib git linc_luajit https://github.com/Psych-Plus-Team/linc_luajit --quiet
 haxelib install hxdiscord_rpc --quiet --skip-dependencies
 haxelib install hxvlc 2.2.5 --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
+haxelib git grig.audio git@github.com:osakared/grig.audio.git
+#haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 echo Finished!
